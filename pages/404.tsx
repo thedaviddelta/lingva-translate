f54bc04d023ca473d7d4de71cedb31497efadb91
David <info@thedaviddelta.com>
2021-06-12 22:29:10 +0200

    Final tweaks

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { CustomError } from "@components";
 
 const My404: FC = () => (
-    <CustomError statusCode={404} statusText={"This page could not be found"} />
+    <CustomError statusCode={404} statusText="This page could not be found" />
 );
 
 export default My404;
